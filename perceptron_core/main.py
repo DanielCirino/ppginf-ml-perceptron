ca07550e2f398b7bd2601000bbb354ea60669959
Daniel Cirino <daniel.cirino@gmail.com>
2024-03-14 18:32:54 -0300

    Implementação da versão inicial do Perceptron

---
+++ perceptron_core/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from perceptron_core.perceptron import Perceptron
+
+
+def plot_decision_boundary(X, y, perceptron):
+    # Plot data points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.bwr, edgecolors='k')
+
+    # Plot decision boundary
+    xlim = plt.gca().get_xlim()
+    ylim = plt.gca().get_ylim()
+    x_values = np.linspace(xlim[0], xlim[1], 100)
+    y_values = np.linspace(ylim[0], ylim[1], 100)
+    xx, yy = np.meshgrid(x_values, y_values)
+    grid_points = np.c_[xx.ravel(), yy.ravel()]
+    Z = np.array([perceptron.predict(point) for point in grid_points])
+    Z = Z.reshape(xx.shape)
+    plt.contour(xx, yy, Z, levels=[0.5], colors='k')
+
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.title('Perceptron Decision Boundary')
+    plt.show()
+
+# Example data
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 1])
+
+# Train perceptron_core
+perceptron = Perceptron(input_size=2)
+perceptron.train(X, y)
+
+# Plot decision boundary
+plot_decision_boundary(X, y, perceptron)
